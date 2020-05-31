8bdadc0f6a153bb767420f10afbf43432cbce58c
Johan Gilces Reyes <jjgilces@espol.edu.ec>
2020-05-31 18:58:19 -0500

    security

--- pagina/src/script/semaforo.js
+++ pagina/src/script/semaforo.js
@@ -3,7 +3,7 @@ let activeLight = 0;
 
 setInterval(() => {
   changeLight();
-}, 2000);
+}, 3000);
 
 function changeLight() {
   circles[activeLight].className = "circle";
@@ -17,3 +17,15 @@ function changeLight() {
 
   currentLight.classList.add(currentLight.getAttribute("color"));
 }
+function changeToy() {
+    circles[activeLight].className = "circle";
+    activeLight++;
+  
+    if (activeLight > 2) {
+      activeLight = 0;
+    }
+  
+    const currentLight = circles[activeLight];
+  
+    currentLight.classList.add(currentLight.getAttribute("color"));
+  }
